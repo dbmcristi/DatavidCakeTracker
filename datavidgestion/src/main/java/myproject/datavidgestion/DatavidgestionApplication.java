c66473488d17e7ffd2b38bf18f7a04dcce086caa
dbmcristi <doducristian1606@gmail.com>
2024-06-14 12:55:25 +0300

    Refactor

--- datavidgestion/src/main/java/myproject/datavidgestion/DatavidgestionApplication.java
+++ datavidgestion/src/main/java/myproject/datavidgestion/DatavidgestionApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DatavidgestionApplication {
-
+//TODO
+	//app.prop settings
+	//resturi
 	public static void main(String[] args) {
 		SpringApplication.run(DatavidgestionApplication.class, args);
 	}
